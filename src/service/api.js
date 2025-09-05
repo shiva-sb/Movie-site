6e72a47c84dbd56a0c35ca5511524618c349fc7e
shiva-sb <shivabala900@gmail.com>
2025-09-05 19:39:55 +0530

    Initial commit

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "4dcb72a1";
-const BASE_URL = "http://www.omdbapi.com/";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_OMDB_BASE_URL;
 
 export const searchMovies = async (query) => {
     const response = await fetch(
